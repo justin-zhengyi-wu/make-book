7c77a6e61c26ee1fddc70fecad74c1f4f4248123
Justin Wu <justin.zhengyi.wu@gmail.com>
2014-03-26 17:34:22 +0800

    Write all of book chapters in one single file.

--- app.js
+++ app.js
@@ -1,112 +1,32 @@
-var request = require('request');
-var fs = require('fs');
-var URL = require('url');
-var QueryString = require('querystring');
-
-var bookInfoUrl = 'http://h5.qidian.com/Book/BookInfo.ashx?bookid=3092598&ajaxMethod=getbookinfo';
-var outputFolder = './output/';
-var bookId = extractBookId(bookInfoUrl);
-var baseUrl = extractUrlBase(bookInfoUrl);
-var bookChaptersUrl = baseUrl + 'Book/BookInfo.ashx?bookid=' + bookId + '&ajaxMethod=getallvolumechapter&isreadvip=0';
-var chapterUrl = baseUrl + 'Book/BookReader.ashx?ajaxMethod=getchapterinfonew&bookId=' + bookId +'&chapterId=';
+var FS = require('fs');
+var fetch = require('./fetch');
 
 
-function extractBookId(url) {
-    var urlObj = URL.parse(url);
-    var queryStrinObj = QueryString.parse(urlObj.query);
-    return queryStrinObj.bookid;
-}
+var bookUrl = 'http://h5.qidian.com/bookinfo.html?bookid=3092598';
+var bookBasePath;
+var bookName;
 
-function extractUrlBase(url) {
-    var urlObj = URL.parse(url);
-    var baseUrl = urlObj.protocol + '//' + urlObj.hostname + (urlObj.port || '') + '/';
-    return baseUrl;
-}
-
-request({url: bookInfoUrl, json: true}, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        if (!body.IsSuccess) {
-            throw new Error(body.ReturnString);
-        }
-        var data = body.ReturnObject[0].ReturnObject;
-        var bookName = data.BookName;
-        var authorName = data.AuthorName;
-
-        request({url: bookChaptersUrl, json: true}, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                if (!body.IsSuccess) {
-                    throw new Error(body.ReturnString);
-                }
-
-                if (!fs.existsSync(outputFolder)) {
-                    fs.mkdirSync(outputFolder);
-                }
-
-                var chapters = body.ReturnObject[1];
-                var bookFolder = outputFolder + bookName + '-' + authorName + '/';
-                if (!fs.existsSync(bookFolder)) {
-                    fs.mkdirSync(bookFolder);
-                }
-                var chaptersData = '';
-                var chaptersUrlArray = [];
-                for (var i = 0, l = chapters.length; i < l; i++) {
-                    var chapterName = chapters[i].ChapterName;
-                    var chapterId = chapters[i].ChapterId;
-                    var chapterFolder = bookFolder + chapterName + '/';
-                    chaptersUrlArray.push({url: chapterUrl + chapterId, name: chapterName});
-                    chaptersData += chapterName + '\n';
-                }
-
-                function sendNextRequest(index) {
-                    if (index < chaptersUrlArray.length) {
-                        var opt = {
-                            url: chaptersUrlArray[index].url,
-                            json: true
-                        };
-
-                        request(opt, function(error, response, body) {
-                            if (!error && response.statusCode == 200) {
-                                var content = body.ReturnObject[0].Content;
-                                content = content.replace(/<p>/g,'\n');
-                                content = content.replace(/<(.)+>(?:.|\n)*<\/\1>/gm, '');
-                                fs.writeFile(bookFolder + chaptersUrlArray[index].name + '.txt', content, function(err) {
-                                    if (err) throw err;
-                                });
-                            }
-                            sendNextRequest(++index);
-                        }); 
-                    }
-                }
-                sendNextRequest(0);
-                
-                for (var j = 0, l = chaptersUrlArray.length; j < l; j++) {
-                    request({url: chaptersUrlArray[j], json: true}, function(error, response, body) {
-                        if (!error && response.statusCode == 200) {
-                            var content = body.ReturnObject.Content;
-                            fs.writeFile(bookFolder + chapterName + '.txt', content, function(err) {
-                                if (err) throw err;
-                            });
-                        }
-                    });
-                }
-
-                fs.writeFile(bookFolder + 'chapters.txt', chaptersData, function(err) {
-                    if (err) {
-                        throw err;
-                    }
-                });
-
-                
-            } else {
-                throw new Error(error);
-            }
-        });
-
-    } else {
-        throw new Error(error);
+function appendNextChapter(lines, i) {
+    var chapter = lines[i].trim();
+    if (chapter) {
+        FS.readFile(bookBasePath + chapter + '.txt', function(err, data) {
+            if (err) {throw err;}
+            var dataWrapper = chapter +'\n' + data.toString() + '\n\n';
+            FS.appendFile('./output/' + bookName, dataWrapper, {encoding: 'utf8', flag: 'a'}, function(err) {
+                if (err) {throw err;}
+                appendNextChapter(lines, ++i);
+            });                
+        });            
     }
+}
+
+fetch.book(bookUrl, function(book) {
+    bookBasePath = './output/' + book.bookName + '-' + book.authorName + '/';
+    bookName = book.bookName + '-' + book.authorName + '.txt';
+    FS.readFile(bookBasePath + 'chapters.txt', function(err, data) {
+        if (err) {throw err;}
+        var lines = data.toString().split('\n');
+        appendNextChapter(lines, 0);
+    });
 });
-
-
-
 
---
+++ fetch.js
@@ -0,0 +1,117 @@
+/*jslint node: true, stupid: true */
+var Request = require('request');
+var FS = require('fs');
+var URL = require('url');
+var QueryString = require('querystring');
+var Events = require('events');
+
+
+exports.book = function(bookUrl, callback) {
+    var eventEmitter = new Events.EventEmitter();
+    var bookName, bookId, authorName, baseUrl, bookInfoUrl, bookChaptersUrl, chapterUrl, outputFolder = './output/';
+
+    function extractBookId(url) {
+        var urlObj = URL.parse(url);
+        var queryStrinObj = QueryString.parse(urlObj.query);
+        return queryStrinObj.bookid;
+    }
+    function extractBaseUrl(url) {
+        var urlObj = URL.parse(url);
+        return urlObj.protocol + '//' + urlObj.hostname + (urlObj.port || '') + '/';
+    }
+    function writeFileCallbackHandler(err) {
+        if (err) {throw err;}    
+    }
+    function sendNextRequestThenWriteFile(bookFolder, chaptersUrlArray, index) {
+        if (index < chaptersUrlArray.length) {
+            var opt = {
+                url: chaptersUrlArray[index].url,
+                json: true
+            };
+            Request(opt, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var content = body.ReturnObject[0].Content;
+                    // Replace the paragraph elements with new lines.
+                    content = content.replace(/<p>/g,'\n');
+                    // Remove the link tags.
+                    content = content.replace(/<a(?:.|\n)*\/a>/gm, '');
+                    FS.writeFile(bookFolder + chaptersUrlArray[index].name + '.txt', content, writeFileCallbackHandler);
+                }
+                sendNextRequestThenWriteFile(bookFolder, chaptersUrlArray, ++index);            
+            }); 
+        } else {
+            eventEmitter.emit('bookDataFetched');
+        }
+    }
+
+
+    bookId = extractBookId(bookUrl);
+    baseUrl = extractBaseUrl(bookUrl);
+    bookInfoUrl = baseUrl + 'Book/BookInfo.ashx?ajaxMethod=getbookinfo&bookid=' + bookId;
+    bookChaptersUrl = baseUrl + 'Book/BookInfo.ashx?ajaxMethod=getallvolumechapter&isreadvip=0&bookid=' + bookId;
+    chapterUrl = baseUrl + 'Book/BookReader.ashx?ajaxMethod=getchapterinfonew&bookId=' + bookId +'&chapterId=';
+
+    Request({url: bookInfoUrl, json: true}, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            if (!body.IsSuccess) {
+                throw new Error(body.ReturnString);
+            }
+            var data = body.ReturnObject[0].ReturnObject;
+            bookName = data.BookName;
+            authorName = data.AuthorName;
+
+            Request({url: bookChaptersUrl, json: true}, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    if (!body.IsSuccess) {
+                        throw new Error(body.ReturnString);
+                    }
+
+                    if (!FS.existsSync(outputFolder)) {
+                        FS.mkdirSync(outputFolder);
+                    }
+
+                    var chapters = body.ReturnObject[1];
+                    var bookFolder = outputFolder + bookName + '-' + authorName + '/';
+                    if (!FS.existsSync(bookFolder)) {
+                        FS.mkdirSync(bookFolder);
+                    }
+
+                    var chaptersData = '';
+                    var chapterName = '';
+                    var chapterId;
+                    var chaptersUrlArray = [];
+                    var i = 0;
+                    var len = chapters.length;
+                    while (i < len) {
+                        chapterName = chapters[i].ChapterName;
+                        chapterId = chapters[i].ChapterId;
+                        chaptersUrlArray.push({url: chapterUrl + chapterId, name: chapterName});
+                        chaptersData += chapterName + '\n';
+                        i++;
+                    }
+
+                    sendNextRequestThenWriteFile(bookFolder, chaptersUrlArray, 0);
+
+                    FS.writeFile(bookFolder + 'chapters.txt', chaptersData, writeFileCallbackHandler);
+                } else {
+                    throw new Error(error);
+                }
+            });
+
+        } else {
+            throw new Error(error);
+        }
+    });
+
+    eventEmitter.on('bookDataFetched', function() {
+        if (callback) {
+            var book = {
+                bookName: bookName,
+                authorName: authorName
+            }
+            callback(book);
+        }    
+    });
+};
+
+
